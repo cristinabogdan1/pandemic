cd2337d1cb344e7d18afc901729ed38f6a5d24e3
EC2 Default User <ec2-user@ip-172-31-40-132.ec2.internal>
2020-10-29 21:55:00 +0000

    X

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Rails.application.routes.draw do
   get '/about' => 'static_pages#about'
 
    get '/contact' => 'static_pages#contact'
+   
+   get '/products' => 'items#index'
+
+
+  get '/pretend_login' => 'user#pretendlogin'
+  
+  get '/pretend_logout' => 'user#pretendlogout'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
